7aefeda2ee0f5422dc84510f36ba6e2bc87f985d
danguyenn <daviddddnguyennnn@gmail.com>
2024-11-05 12:00:33 -0800

    Clean up code & documentation

--- train.py
+++ train.py
@@ -4,32 +4,20 @@ import torch
 import os
 from PIL import Image
 
-def run():
+def train():
     torch.multiprocessing.freeze_support()
-    
-    # Initialize Roboflow
-    #rf = Roboflow(api_key="PonCX4D1tGssuHy2wkxd")
-    #project = rf.workspace("roses").project("fetal-hc")
-    #version = project.version(1)
-    #dataset = version.download("yolov11")
 
-
-    #rf = Roboflow(api_key="PonCX4D1tGssuHy2wkxd")
-    #project = rf.workspace("mhf-model").project("training-set-gqfju-wv7jx")
-    #version = project.version(1)
-    #dataset = version.download("yolov11")
-    
-
+    #Import dataset from Roboflow
     rf = Roboflow(api_key="PonCX4D1tGssuHy2wkxd")
     project = rf.workspace("mhf-model").project("training-set-gqfju-wv7jx")
     version = project.version(2)
     dataset = version.download("yolov11")
                 
-                
     # Check if CUDA is available
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     print(f"Using device: {device}")
 
+    #Print CUDA information if available
     print("CUDA available:", torch.cuda.is_available())
     print("CUDA version:", torch.version.cuda)
     print("Number of GPUs:", torch.cuda.device_count())
@@ -52,31 +40,25 @@ def test():
 
     # Run batched inference on a list of images
     results = model([os.path.join("training-set-2/test/images", filename) for filename in os.listdir("training-set-2/test/images")])
-   
-    ## Process results list
-    #for result in results:
-    #    boxes = result.boxes  # Boxes object for bounding box outputs
-    #    masks = result.masks  # Masks object for segmentation masks outputs
-    #    keypoints = result.keypoints  # Keypoints object for pose outputs
-    #    probs = result.probs  # Probs object for classification outputs
-    #    obb = result.obb  # Oriented boxes object for OBB outputs
-    #    #result.show()  # display to screen
-    #    result.save(filename="result.jpg")  # save to disk
         
     # Directory paths
     test_images_dir = "training-set-2/test/images"
     
-    #create a new results file if it exists
+    # Create a new results file if it already exists, increment folder number to distinguish new tests
     result_images_dir = "result-images-0"
     counter = 1
+    # Incrementing to update folder name by version
     while(os.path.exists(result_images_dir)):
         result_images_dir = result_images_dir[:len(result_images_dir) - 1] + str(counter)
         counter += 1
     
-    
+    # Make a new folder directory
     os.makedirs(result_images_dir)
         
+    # Add all test images into array in image_filenames
     image_filenames = [filename for filename in os.listdir(test_images_dir) if filename.endswith(('.png', '.jpg', '.jpeg'))]
+    
+    # Run test images through model to get predictions based on pretrained model weights
     results = model([os.path.join(test_images_dir, filename) for filename in image_filenames])
     
     # Process results list
@@ -98,6 +80,7 @@ def test():
         # Save the combined image
         combined_image.save(os.path.join(result_images_dir, f"combined_{image_filenames[i]}"))
 
+# Helper function to visualize predictions and compare side-by-side with test images
 def combine_images_side_by_side(test_image_path, result_image_path):
     # Open the test and result images
     test_image = Image.open(test_image_path)
@@ -116,6 +99,8 @@ def combine_images_side_by_side(test_image_path, result_image_path):
     combined_image.paste(result_image, (test_image.width, 0))
 
     return combined_image
+
+# Main
 if __name__ == '__main__':
-    #run()
+    #train()
     test()
